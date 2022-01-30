f1274933d17352e2e5df892d1583561644a7dd84
javacoding007 <98701091+javacoding007@users.noreply.github.com>
2022-01-30 16:32:05 +0000

    Create AreaOfRectangle.java

---
+++ Day-1-Assessment/AreaOfRectangle.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class AreaOfRectangle
+{
+	public static void main(String[] args) 
+	{
+	    Scanner sc = new Scanner(System.in);
+	    int l = sc.nextInt();
+	    int b = sc.nextInt();
+	    System.out.println(l * b);
+	}
+}
