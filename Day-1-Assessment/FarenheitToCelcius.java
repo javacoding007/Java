b812dd3bf06bd706b212103b908ef362d5eb1510
javacoding007 <98701091+javacoding007@users.noreply.github.com>
2022-01-30 16:13:44 +0000

    Update FarenheitToCelcius.java

--- Day-1-Assessment/FarenheitToCelcius.java
+++ Day-1-Assessment/FarenheitToCelcius.java
@@ -6,6 +6,6 @@ public class Main
 	    Scanner sc = new Scanner(System.in);
 	    int f = sc.nextInt();
 	    float c = (f - 32)*5.0f / 9.0f;
-      System.out.println(c);
+            System.out.println(c);
 	}
 }
