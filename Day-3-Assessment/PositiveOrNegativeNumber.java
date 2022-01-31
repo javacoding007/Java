4d4b3fb712c390cd5d6d09608759378ae20b170c
javacoding007 <98701091+javacoding007@users.noreply.github.com>
2022-01-31 07:29:45 +0000

    Create PositiveOrNegativeNumber.java

---
+++ Day-3-Assessment/PositiveOrNegativeNumber.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class PositiveOrNegativeNumber
+{
+	public static void main(String[] args)
+	{
+	    Scanner sc = new Scanner(System.in);
+	    int a = sc.nextInt();
+	    if(a > 0)
+	    {
+	        System.out.println("Positive");
+	    }
+	    else if(a < 0)
+	    {
+	        System.out.println("Negative");
+	    }
+	    else
+	    {
+	        System.out.println("Zero");
+	    }
+	}    
+}
