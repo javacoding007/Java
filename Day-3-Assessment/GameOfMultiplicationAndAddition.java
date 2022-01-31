fe2a34190c30225cd620b566ab3e0d13a7c66d4c
javacoding007 <98701091+javacoding007@users.noreply.github.com>
2022-01-31 07:28:22 +0000

    Create GameOfMultiplicationAndAddition.java

---
+++ Day-3-Assessment/GameOfMultiplicationAndAddition.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class GameOfMultiplicationAndAddition
+{
+	public static void main(String[] args)
+	{
+	    Scanner sc = new Scanner(System.in);
+	    int a = sc.nextInt();
+	    int b = sc.nextInt();
+	    if(a % 2 == 0 && b % 2 == 0)
+	    {
+	        System.out.println(a * b);
+	    }
+	    else
+	    {
+	        System.out.println(a + b);
+	    }
+    }
+}
